497c071f19f20030bdd9940a7381fb9053380f94
Mahmoud3adel21 <mahmoud3adel21@gmail.com>
2023-01-03 05:32:09 +0200

    add featured listView to home

---
+++ lib/features/home/presentation/view/widgets/featuredbook_listview.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import 'custom_listview_item.dart';
+
+class FeaturedBookListView extends StatelessWidget {
+  const FeaturedBookListView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * .3,
+      child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemBuilder: ((context, index) {
+            return const Padding(
+              padding:  EdgeInsets.symmetric(horizontal: 6),
+              child:  FeaturedListViewItem(),
+            );
+          })),
+    );
+  }
+}

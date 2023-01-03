1afad170f009c168b509ab0339ef6dc8861d57d6
Mahmoud3adel21 <mahmoud3adel21@gmail.com>
2023-01-03 05:32:35 +0200

    add Featured listView item

--- lib/features/home/presentation/view/widgets/home_view_body.dart
+++ lib/features/home/presentation/view/widgets/home_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'custom_appbar.dart';
+import 'featuredbook_listview.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({Key? key}) : super(key: key);
@@ -9,6 +10,7 @@ class HomeViewBody extends StatelessWidget {
     return Column(
       children: const [
         CustomAppBar(),
+        FeaturedBookListView(),
       ],
     );
   }
